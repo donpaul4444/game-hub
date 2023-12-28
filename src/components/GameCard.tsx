54a025111dbfdc1ac3e0f8c06efad1e5f3c33a38
donpaul4444 <donpaul4444@gmail.com>
2023-12-28 22:51:05 +0530

    Refactor:remove duplicated styles

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,7 +11,7 @@ game:Game
 
 const GameCard = ({game}:Props) => {
   return (
-    <Card width='300px'  borderRadius={10} overflow='hidden'>
+    <Card >
         <Image src={getCroppedImageUrl( game.background_image)}></Image>
         <CardBody>
             <Heading fontSize='2xl'>{game.name}</Heading>
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,23 +1,34 @@
-
-import { SimpleGrid, Text } from '@chakra-ui/react';
-import useGames from '../hooks/useGames';
-import GameCard from './GameCard';
-import GameCardSkeleton from './GameCardSkeleton';
+import { SimpleGrid, Text } from "@chakra-ui/react";
+import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
-    const{games,error, isLoading}=useGames()
-    const skeletons=[1,2,3,4,5,6,7,8,9,10]
+  const { games, error, isLoading } = useGames();
+  const skeletons = [1, 2, 3, 4, 5, 6,7,8,9,10];
   return (
-   <>
-   {error&&<Text>{error}</Text>}
-    <SimpleGrid columns={{sm:1, md:2,lg:3, xl:5 }}padding='10px' spacing={10}>
-       {isLoading&&skeletons.map(skeleton=><GameCardSkeleton key={skeleton}/>)}
-        {games.map((game)=>(
-            <GameCard key={game.id} game={game}/>
+    <>
+      {error && <Text>{error}</Text>}
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        padding="10px"
+        spacing={10}
+      >
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <GameCardContainer>
+              <GameCardSkeleton key={skeleton} />
+            </GameCardContainer>
+          ))}
+        {games.map((game) => (
+          <GameCardContainer>
+            <GameCard key={game.id} game={game} />
+          </GameCardContainer>
         ))}
-    </SimpleGrid>
-   </>
-  )
-}
+      </SimpleGrid>
+    </>
+  );
+};
 
-export default GameGrid
+export default GameGrid;
